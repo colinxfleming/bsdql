cd1ae12557f2f8629fe0303ad965cbd607c0377d
Colin Fleming <c3flemin@gmail.com>
2014-12-14 20:56:03 -0500

    change dirs

---
+++ readme.md
@@ -0,0 +1,7 @@
+<h1>Text</h1>
+
+<p>Explanation</p>
+
+<h2>Breakout</h2>
+
+<h2>More Breakout</h2>
---
+++ table_structure/signups.sql
@@ -0,0 +1,59 @@
+-- to drill down from stg_signup to cons_id: stg_signup > cons_action_signup > cons_action
+
+select 
+	ca.cons_id, s.email
+from stg_signup s 
+join cons_action_signup cas
+	on s.stg_signup_id = cas.stg_signup_id
+join cons_action ca 
+	on cas.cons_action_signup_id = ca.cons_action_signup_id
+;
+
+-- 
+/* 
+
+It's best to filter on keys in stg_signup, in my experience. Some of the keys drop off or act weird when you drill in further. 
+
+AVAILABLE AND USEFUL FILTERS IN SIGNUPS
+- chapter_id
+- create_dt
+- source 
+- subsource
+
+*/
+
+-- sample queries using each of these filters: 
+
+-- signups just in chapters 2 and 3
+select 
+	ca.cons_id, s.email
+from stg_signup s 
+join cons_action_signup cas
+	on s.stg_signup_id = cas.stg_signup_id
+join cons_action ca 
+	on cas.cons_action_signup_id = ca.cons_action_signup_id
+where s.chapter_id in (2,3)
+;
+
+-- signups after January 1, 2014
+select 
+	ca.cons_id, s.email
+from stg_signup s 
+join cons_action_signup cas
+	on s.stg_signup_id = cas.stg_signup_id
+join cons_action ca 
+	on cas.cons_action_signup_id = ca.cons_action_signup_id
+where s.create_dt >= '2014-01-01'
+;
+
+-- signups with the source 'michigan_best_state' and subsource 'jennifer_granholm'
+select 
+	ca.cons_id, s.email
+from stg_signup s 
+join cons_action_signup cas
+	on s.stg_signup_id = cas.stg_signup_id
+join cons_action ca 
+	on cas.cons_action_signup_id = ca.cons_action_signup_id
+where s.source = 'michigan_best_state'
+	and s.subsource = 'jennifer_granholm'
+;
